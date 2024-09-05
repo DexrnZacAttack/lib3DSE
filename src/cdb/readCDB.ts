333f1d4264a38470f25bbbfac570ea8f50f2ac0d
ZacAttack <dexrn@hotmail.com>
2024-09-04 17:22:31 -0700

    hmmmm

--- src/cdb/readCDB.ts
+++ src/cdb/readCDB.ts
@@ -41,7 +41,7 @@ export async function readCDB(cdb: Uint8Array): Promise<Chunk[]> {
     let loopCount = 0;
 
     const reader: bReader = bReaderFromBuf(cdb, true);
-    
+
     const chunks: Chunk[] = [];
 
     // read initial stuff
@@ -61,55 +61,51 @@ export async function readCDB(cdb: Uint8Array): Promise<Chunk[]> {
     for (let i = 0; i < initFileCount; i++) {
         files.push(cdb.slice(i * initFileSize, i * initFileSize + initFileSize));
     }
-    
 
     await Promise.all(files.map(async file => {
-    loopCount++;
-    const fReader = bReaderFromBuf(file, true);
-    console.log(fReader);
-    // smh I really need to finish my binary tools
-    fReader.readShort();
-    fReader.readShort();
-    fReader.readUInt();
-    fReader.readUInt();
-    fReader.readUInt();
-    fReader.readUInt();
+        loopCount++;
+        const fReader = bReaderFromBuf(file, true);
+            // smh I really need to finish my binary tools
+            fReader.readShort();
+            fReader.readShort();
+            fReader.readUInt();
+            fReader.readUInt();
+            fReader.readUInt();
+            fReader.readUInt();
 
-    const magic = (fReader.readUInt()).toString(16);
-    if (magic != "abcdef98")
-        throw new TypeError(`Magic "${magic}" does not match expected magic "abcdef98"`);
+            const magic = (fReader.readUInt()).toString(16);
+            if (magic != "abcdef98")
+                throw new TypeError(`Magic "${magic}" does not match expected magic "abcdef98"`);
 
 
-    fReader.readUInt();
-    fReader.readUInt();
-    fReader.readUInt();
+            fReader.readUInt();
+            fReader.readUInt();
+            fReader.readUInt();
 
-    const chunkSections: ChunkSection[] = [];
+            const chunkSections: ChunkSection[] = [];
 
-    for (var j = 0; j < 6; j++) {
-        const index = fReader.readInt();
-        const compressedSize = fReader.readInt();
-        const decompressedSize = fReader.readInt();
-        chunkSections.push({index, compressedSize, decompressedSize});
-    }
+            for (var j = 0; j < 6; j++) {
+                const index = fReader.readInt();
+                const compressedSize = fReader.readInt();
+                const decompressedSize = fReader.readInt();
+                chunkSections.push({ index, compressedSize, decompressedSize });
+            }
 
-    for (const chunkSection of chunkSections) {
-        if (chunkSection.index === -1) {
-            return;
-        }
+            for (const chunkSection of chunkSections) {
+                if (chunkSection.index === -1) {
+                    return;
+                }
 
-        console.log(chunkSection.index)
-        fReader.setPos(chunkSection.index)
+                // This does something unwanted, was this debug code?
+                fReader.setPos(chunkSection.index);
 
-        console.log(fReader.pos);
-
-        const fullChunk = fReader.slice(fReader.pos, fReader.pos + initFileSize);
-        const chunk = fullChunk.slice(0, chunkSection.compressedSize);
-        const dcChunk = await decompress(new Uint8Array(chunk), "deflate");
-        console.log(fReader.pos);
-        chunks.push({ index: chunkSection.index, size: dcChunk.length, data: dcChunk })
-    };
-}));
+                const fullChunk = fReader.slice(fReader.pos, fReader.pos + initFileSize);
+                const chunk = fullChunk.slice(0, chunkSection.compressedSize);
+                const dcChunk = await decompress(new Uint8Array(chunk), "deflate");
+                console.log({ index: chunkSection.index, size: dcChunk.length, data: dcChunk });
+                chunks.push({ index: chunkSection.index, size: dcChunk.length, data: dcChunk })
+            };
+    }));
 
     return chunks;
 }
